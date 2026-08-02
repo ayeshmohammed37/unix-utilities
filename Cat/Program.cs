021082a8df57a34a34d72f218214d3ca6e2d9c78
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-08-02 22:02:59 +0300

    Cat: open file int path and write the content to the standard out

--- Cat/Program.cs
+++ Cat/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Step 1: open the file specified on the command line and write its contents to standard out.
+            string path = "C:\\Users\\ayesh\\ayesh_work\\Dev\\unix-utilities\\TestData\\test.txt";
+
+            string data = File.ReadAllText(path);
+
+            Console.Write(data);
         }
     }
 }
