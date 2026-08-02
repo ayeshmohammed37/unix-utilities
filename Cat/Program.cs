34e46f3c0eaf08139834b199896c2484718f3891
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-08-02 23:01:16 +0300

    Cat: Handle command line args

--- Cat/Program.cs
+++ Cat/Program.cs
@@ -5,11 +5,17 @@
         static void Main(string[] args)
         {
             // Step 1: open the file specified on the command line and write its contents to standard out.
-            string path = "C:\\Users\\ayesh\\ayesh_work\\Dev\\unix-utilities\\TestData\\test2.txt";
+            
+            
 
             try
             {
-                string data = File.ReadAllText(path);
+                if (args.Length != 1)
+                {
+                    throw new Exception("Invalid Parameters");
+                }
+
+                string data = File.ReadAllText(args[0]);
                 Console.WriteLine(data);
             }
             catch (Exception ex)
