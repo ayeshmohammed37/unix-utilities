e0979ff7c92e9bce8cae63ccb8cc8aae3618367b
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-08-03 07:42:41 +0300

    Cat/Step2: Reading input from standard in

--- Cat/Program.cs
+++ Cat/Program.cs
@@ -4,34 +4,44 @@
     {
         static void Main(string[] args)
         {
-            if (args.Length != 1)
+            // Step 1
+            if (args.Length == 1 && !Console.IsInputRedirected)
+            {
+                string filePath = args[0];
+                if (!File.Exists(filePath))
+                {
+                    Console.Error.WriteLine($"cat: {filePath}: no such file or directory");
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                try
+                {
+                    using Stream sourceStream = File.OpenRead(filePath);
+                    using Stream standardOut = Console.OpenStandardOutput();
+
+                    sourceStream.CopyTo(standardOut);
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine($"cat: {filePath}: {ex.Message}");
+                    Environment.ExitCode = 1;
+                }
+            }
+            // Step 2
+            else if (args.Length == 0 && Console.IsInputRedirected)
+            {
+                string standardInData = Console.In.ReadToEnd();
+                Console.Write(standardInData);
+
+            }
+            else
             {
                 Console.Error.WriteLine("Usage: cat <file_path>");
                 Environment.ExitCode = 1;
                 return;
             }
-
-            string path = args[0];
-
-            if (!File.Exists(path))
-            {
-                Console.Error.WriteLine($"cat: {path}: no such file or directory");
-                Environment.ExitCode = 1;
-                return;
-            }
-
-            try
-            {
-                using Stream sourceStream = File.OpenRead(path);
-                using Stream standardOut = Console.OpenStandardOutput();
-
-                sourceStream.CopyTo(standardOut);
-            }
-            catch (Exception ex)
-            {
-                Console.Error.WriteLine($"cat: {path}: {ex.Message}");
-                Environment.ExitCode = 1;
-            }
         }
+
     }
 }
